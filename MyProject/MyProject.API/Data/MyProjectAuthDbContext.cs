8dbe2700553edd050e8b2d9d7e000b8dc25fce20
Nikhilesh <nikhileshyadavkk@gmail.com>
2026-05-19 15:32:36 +0530

    seeding roles

--- MyProject/MyProject.API/Data/MyProjectAuthDbContext.cs
+++ MyProject/MyProject.API/Data/MyProjectAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace MyProject.API.Data
@@ -8,6 +9,32 @@ namespace MyProject.API.Data
         public MyProjectAuthDbContext(DbContextOptions<MyProjectAuthDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = Guid.Parse("1a2b3c4d-5e6f-7a8b-9c0d-1e2f3a4b5c6d").ToString();
+            var writerRoleId = Guid.Parse("2b3c4d5e-6f7a-8b9c-0d1e-2f3a4b5c6d7e").ToString();
+            var roles = new List<IdentityRole>()
+            {
+                new IdentityRole
+                {
+                    Id=readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name="Reader",
+                    NormalizedName="Reader".ToUpper(),
+                },
+                new IdentityRole
+                {
+                    Id=writerRoleId,
+                    ConcurrencyStamp= writerRoleId,
+                    Name="Writer",
+                    NormalizedName="Writer".ToUpper()
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
